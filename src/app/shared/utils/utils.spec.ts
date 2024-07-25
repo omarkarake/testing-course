9927b4901c0ddf16eeb8d7b9cfef64af553ddf44
omar karake <omar.karake@amalitechtraining.org>
2024-07-25 11:07:32 +0200

    describe is our section and it is our test

---
+++ src/app/shared/utils/utils.spec.ts
@@ -0,0 +1,30 @@
+// import { pluck, range } from './utils';
+
+// describe('utils', () => {
+//   describe('range', () => {
+//     it('returns correct range from 1 to 5', () => {
+//       expect(range(1, 5)).toEqual([1, 2, 3, 4]);
+//     });
+//     it('returns correct range from 41 to 44', () => {
+//       expect(range(41, 44)).toEqual([41, 42, 43]);
+//     });
+//   });
+
+//   describe('pluck', () => {
+//     it('returns correct result', () => {
+//       const data = [
+//         { id: '1', name: 'foo' },
+//         { id: '2', name: 'bar' },
+//         { id: '3', name: 'baz' },
+//       ];
+//       expect(pluck(data, 'id')).toEqual(['1', '2', '3']);
+//     });
+//   });
+// });
+
+describe('utils', () => {
+  // describe is our section and it is our test
+  it('should 1 be 1', () => {
+    expect(1).toEqual(1);
+  });
+});
