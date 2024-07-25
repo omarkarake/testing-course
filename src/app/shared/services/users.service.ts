c19289256e9823b046d8cd9120e6b4e07944eabe
omar karake <omar.karake@amalitechtraining.org>
2024-07-25 12:27:01 +0200

    injecting service in another service

--- src/app/shared/services/users.service.ts
+++ src/app/shared/services/users.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@angular/core';
+import { UtilsService } from './utils.service';
+import { inject, Injectable } from '@angular/core';
 import { UserInteface } from '../types/user.interface';
 
 @Injectable()
 export class UsersService {
+  utilsService = inject(UtilsService);
   users: UserInteface[] = [];
 
   addUser(user: UserInteface): void {
@@ -12,5 +14,9 @@ export class UsersService {
   removeUser(userId: string): void {
     const updatedUsers = this.users.filter((user) => userId !== user.id);
     this.users = updatedUsers;
+  }
+
+  getUsername(): string[] {
+    return this.utilsService.pluck(this.users, 'name');
   }
 }
