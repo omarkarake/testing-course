755c477b0cb9d662849f35836071c0dcdbf2b451
omar karake <omar.karake@amalitechtraining.org>
2024-07-28 15:26:02 +0200

    testing simple angular component

--- src/app/shared/components/errorMessage/errorMessage.component.spec.ts
+++ src/app/shared/components/errorMessage/errorMessage.component.spec.ts
@@ -2,8 +2,9 @@
 // import { ErrorMessageComponent } from './errorMessage.component';
 // import { By } from '@angular/platform-browser';
 
-import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { ErrorMessageComponent } from "./errorMessage.component";
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ErrorMessageComponent } from './errorMessage.component';
+import { By } from '@angular/platform-browser';
 
 // describe('ErrorMessageComponent', () => {
 //   let component: ErrorMessageComponent;
@@ -50,10 +51,33 @@ describe('errorMessageComponent', () => {
   let fixture: ComponentFixture<ErrorMessageComponent>;
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ErrorMessageComponent]
+      imports: [ErrorMessageComponent],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ErrorMessageComponent);
     component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('create a component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('render default error state', () => {
+    const messageContainer = fixture.debugElement.query(
+      By.css('[data-testid="message-container"]') // this will look element with attribute like thatssss
+    );
+    expect(messageContainer.nativeElement.textContent).toEqual(
+      'Something went wrong'
+    );
+  });
+
+  it('render custom error message', () => {
+    component.message = 'Email is already taken';
+    fixture.detectChanges();
+    const messageContainer = fixture.debugElement.query(
+      By.css('[data-testid="message-container"]')
+    );
+    expect(messageContainer.nativeElement.textContent).toEqual('Email is already taken')
   });
 });
