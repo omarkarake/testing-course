efafb9f6dbc8b99a478094a86e14854c3a2cb407
omar karake <omar.karake@amalitechtraining.org>
2024-07-29 08:53:55 +0200

    testing our pagination component

---
+++ src/app/shared/components/pagination/pagination.component.spec.ts
@@ -0,0 +1,96 @@
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { PaginationComponent } from './pagination.component';
+// import { UtilsService } from '../../services/utils.service';
+// import { By } from '@angular/platform-browser';
+// import { first } from 'rxjs';
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PaginationComponent } from './pagination.component';
+import { UtilsService } from '../../services/utils.service';
+import { By } from '@angular/platform-browser';
+import { first } from 'rxjs';
+
+// describe('PaginationComponent', () => {
+//   let component: PaginationComponent;
+//   let fixture: ComponentFixture<PaginationComponent>;
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       imports: [PaginationComponent],
+//       providers: [UtilsService],
+//     }).compileComponents();
+
+//     fixture = TestBed.createComponent(PaginationComponent);
+//     component = fixture.componentInstance;
+//     component.total = 50;
+//     component.limit = 10;
+//     component.currentPage = 1;
+//     fixture.detectChanges();
+//   });
+
+//   it('creates component', () => {
+//     expect(component).toBeTruthy();
+//   });
+
+//   it('renders correct pagination', () => {
+//     const pageContainers = fixture.debugElement.queryAll(
+//       By.css('[data-testid="page-container"]')
+//     );
+
+//     expect(pageContainers.length).toBe(5);
+//     expect(pageContainers[0].nativeElement.textContent).toContain('1');
+//   });
+
+//   it('should emit a clicked page', () => {
+//     const pageContainers = fixture.debugElement.queryAll(
+//       By.css('[data-testid="page-container"]')
+//     );
+//     let clickedPage: number | undefined;
+
+//     component.pageChangeEvent.pipe(first()).subscribe((page) => {
+//       clickedPage = page;
+//     });
+//     pageContainers[0].triggerEventHandler('click');
+//     expect(clickedPage).toEqual(1);
+//   });
+// });
+
+describe('paginationComponent', () => {
+  let component: PaginationComponent;
+  let fixture: ComponentFixture<PaginationComponent>;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [PaginationComponent],
+      providers: [UtilsService],
+    }).compileComponents();
+    fixture = TestBed.createComponent(PaginationComponent);
+    component = fixture.componentInstance;
+    component.total = 50;
+    component.limit = 10;
+    component.currentPage = 1;
+    fixture.detectChanges();
+  });
+
+  it('creates component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('render correct pagination', () => {
+    const pageContainer = fixture.debugElement.queryAll(
+      By.css('[data-testid="page-container"]')
+    );
+    expect(pageContainer.length).toBe(5);
+    expect(pageContainer[0].nativeElement.textContent).toContain('1');
+  });
+
+  it('should emit a clicked page', () => {
+    const pageContainer = fixture.debugElement.queryAll(
+      By.css('[data-testid="page-container"]')
+    );
+    let clickedPage: number | undefined;
+    component.pageChangeEvent
+      .pipe(first())
+      .subscribe((page) => (clickedPage = page));
+    pageContainer[0].triggerEventHandler('click');
+    expect(clickedPage).toEqual(1);
+  });
+});
