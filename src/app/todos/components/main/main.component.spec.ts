0d825404a866bb88d0e32c6f3c148fbf7ecd3b7e
omar karake <omar.karake@amalitechtraining.org>
2024-07-29 12:21:32 +0200

    testing main after error

--- src/app/todos/components/main/main.component.spec.ts
+++ src/app/todos/components/main/main.component.spec.ts
@@ -1,8 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MainComponent } from './main.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TodosService } from '../../services/todos.service';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { TodoInterface } from '../../types/todo.interface';
 import { TodoComponent } from '../todo/todo.component';
 import { By } from '@angular/platform-browser';
@@ -19,10 +19,11 @@ class TodoComponentMock {
   @Output() setEditingId: EventEmitter<string | null> = new EventEmitter();
 }
 
-describe('mainComponent', () => {
+describe('MainComponent', () => {
   let component: MainComponent;
   let fixture: ComponentFixture<MainComponent>;
   let todosService: TodosService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [MainComponent, HttpClientTestingModule],
@@ -32,19 +33,22 @@ describe('mainComponent', () => {
         add: { imports: [TodoComponentMock] },
       })
       .compileComponents();
+
     fixture = TestBed.createComponent(MainComponent);
     component = fixture.componentInstance;
     todosService = TestBed.inject(TodosService);
+    fixture.detectChanges();
   });
+
   it('creates a component', () => {
     expect(component).toBeTruthy();
   });
 
   describe('component visibility', () => {
     it('should be hidden without todos', () => {
-        const main = fixture.debugElement.query(By.css('[data-testid="main"]'));
-        expect(main.classes['hidden']).toEqual(true);
-      });
+      const main = fixture.debugElement.query(By.css('[data-testid="main"]'));
+      expect(main.classes['hidden']).toEqual(true);
+    });
 
     it('should be visible with todos', () => {
       todosService.todosSig.set([{ id: '1', text: 'foo', isCompleted: true }]);
